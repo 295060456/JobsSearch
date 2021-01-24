2f5a03fe07ea082837ee9bf95b4ef78300643a96
Jobs <lg295060456@gmail.com>
2021-01-24 14:06:45 +0700

    no message

--- JobsSearch/3rd/BaseCustomizeUIKit/UIColor/UIColor+Hex.h
+++ JobsSearch/3rd/BaseCustomizeUIKit/UIColor/UIColor+Hex.h
@@ -8,9 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-#define RGBA_COLOR(R, G, B, A) [UIColor colorWithRed:((R) / 255.0f) green:((G) / 255.0f) blue:((B) / 255.0f) alpha:A]
-#define RGB_COLOR(R, G, B) [UIColor colorWithRed:((R) / 255.0f) green:((G) / 255.0f) blue:((B) / 255.0f) alpha:1.0f]
-
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIColor (Hex)
