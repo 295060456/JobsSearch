0607b92a2c1dd2d602882459602cef206b922a96
Jobs <lg295060456@gmail.com>
2021-01-27 12:10:42 +0700

    no message

--- JobsSearch/3rd/BaseCustomizeUIKit/UICollectionView/UICollectionView+RegisterClass.m
+++ JobsSearch/3rd/BaseCustomizeUIKit/UICollectionView/UICollectionView+RegisterClass.m
@@ -11,36 +11,20 @@
 
 -(void)RegisterClass{
     
-//    [self registerClass:UBLSearchCollectionReusableView.class
-//        forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
-//               withReuseIdentifier:@"UBLSearchCollectionReusableView"];
-//    [self registerClass:UBLHotCollectionReusableView.class
-//        forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
-//               withReuseIdentifier:@"UBLHotCollectionReusableView"];
-//    [self registerClass:UBLAllCollectionReusableView.class
-//        forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
-//               withReuseIdentifier:@"UBLAllCollectionReusableView"];
-//    
-//    [self registerClass:UBLVideoCollectionViewCell_Style1.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style1"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style2.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style2"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style3.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style3"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style4.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style4"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style5.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style5"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style6.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style6"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style7.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style7"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style8.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style8"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style9.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style9"];
-//    [self registerClass:UBLVideoCollectionViewCell_Style10.class
-//        forCellWithReuseIdentifier:@"UBLVideoCollectionViewCell_Style10"];
+    //    [self registerClass:DDUserDetailsCollectionReusableView.class
+    //        forSupplementaryViewOfKind:UICollectionElementKindSectionHeader
+    //        withReuseIdentifier:NSStringFromClass(DDUserDetailsCollectionReusableView.class)];
+    //
+    //    [self registerClass:DDCollectionViewCell_Style1.class
+    //        forCellWithReuseIdentifier:NSStringFromClass(DDCollectionViewCell_Style1.class)];
+    //    [self registerClass:DDCollectionViewCell_Style2.class
+    //        forCellWithReuseIdentifier:NSStringFromClass(DDCollectionViewCell_Style2.class)];
+    //    [self registerClass:DDCollectionViewCell_Style3.class
+    //        forCellWithReuseIdentifier:NSStringFromClass(DDCollectionViewCell_Style3.class)];
+    //    [self registerClass:DDCollectionViewCell_Style4.class
+    //        forCellWithReuseIdentifier:NSStringFromClass(DDCollectionViewCell_Style4.class)];
+    //    [self registerClass:DDCollectionViewCell_Style5.class
+    //        forCellWithReuseIdentifier:NSStringFromClass(DDCollectionViewCell_Style5.class)];
 }
 
 @end
