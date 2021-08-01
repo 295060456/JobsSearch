7ecce2a0fbbd77da98e352885d75f831bf6e456e
Jobs <lg295060456@vip.qq.com>
2021-08-01 13:51:53 +0400

    no message

---
+++ JobsSearch/🌹Macro/3rdManager/CocoaPods/PodsManual.h
@@ -0,0 +1,33 @@
+//
+//  PodsManual.h
+//  Shooting
+//
+//  Created by Jobs on 2020/12/16.
+//  Copyright © 2020 Jobs. All rights reserved.
+//
+
+#ifndef PodsManual_h
+#define PodsManual_h
+
+#if __has_include(<XHLaunchAd/XHLaunchAd.h>)
+#import <XHLaunchAd/XHLaunchAd.h>
+#else
+#import "XHLaunchAd.h"
+#endif
+
+#if __has_include(<ZZCircleProgress/ZZCircleProgress.h>)
+#import <ZZCircleProgress/ZZCircleProgress.h>
+#else
+#import "ZZCircleProgress.h"
+#endif
+/// 鉴权
+#import "ECPrivacyCheckTools.h"
+/// GKPhotoBrowser
+#import "GKPhotoBrowser+SaveBtn.h"
+/// JXCategoryView
+#import "JXCategoryTitleBackgroundView.h"
+#import "JXCategoryTimelineView.h"
+/// 提示框
+#import "WHToast+DDToast.h"
+
+#endif /* PodsManual_h */
